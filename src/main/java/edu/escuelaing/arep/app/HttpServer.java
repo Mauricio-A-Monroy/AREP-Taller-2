37ff52556c406bce284af28460415e439e196596
Mauricio Monroy <mauricio.monroy-j@mail.escuelaing.edu.co>
2025-02-06 09:25:52 -0500

    commit

--- src/main/java/edu/escuelaing/arep/app/HttpServer.java
+++ src/main/java/edu/escuelaing/arep/app/HttpServer.java
@@ -8,6 +8,7 @@ import java.util.function.BiFunction;
 
 public class HttpServer {
     private static Map<String,BiFunction<HttpRequest, HttpResponse, String>> servicios = new HashMap();
+    private static String staticFilePath = "src/main/resources/static";;
 
     public static void start() throws IOException, URISyntaxException {
         ServerSocket serverSocket = null;
@@ -19,6 +20,9 @@ public class HttpServer {
         }
 
         boolean running = true;
+        int idCounter = 0;
+        Map<Integer, String> dataStore = new HashMap<>();
+
         while (running) {
 
             Socket clientSocket = null;
@@ -57,14 +61,22 @@ public class HttpServer {
             System.out.println("Request: " + HTTPRequest);
             System.out.println("FilePath: " + filePath);
 
-            URI resourceuri = new URI(filePath);
-            System.out.println("URI: " + resourceuri);
+            URI resourceURI = new URI(filePath);
+            System.out.println("URI: " + resourceURI);
 
-            if (resourceuri.getPath().startsWith("/app/")) {
-                HttpRequest req = new HttpRequest(resourceuri.getPath(), resourceuri.getQuery());
-                HttpResponse resp = new HttpResponse();
-                outputLine = processRequest(req, resp);
-                out.println(outputLine);
+            if (resourceURI.getPath().startsWith("/app/")) {
+                if(resourceURI.getPath().endsWith("rest-service")){
+                    handleRestRequest(HTTPRequest, filePath, resourceURI,out, dataStore, idCounter);
+                    if (HTTPRequest.equals("POST")) {
+                        idCounter += 1;
+                    }
+                }
+                else {
+                    HttpRequest req = new HttpRequest(resourceURI.getPath(), resourceURI.getQuery());
+                    HttpResponse resp = new HttpResponse();
+                    outputLine = processRequest(req, resp);
+                    out.println(outputLine);
+                }
             } else {
                 getDefaultResponse(filePath, out, dataOut);
             }
@@ -88,17 +100,20 @@ public class HttpServer {
         String response = "HTTP/1.1 200 OK\r\n"
                 + "Content-Type: application/json\r\n"
                 + "\r\n"
-                + "{\"response\":\""+ s.apply(req, resp) +"\"}";
+                + "{\"response\":\"" + s.apply(req, resp) + "\"}";
         return response;
+
     }
-    public static void staticfiles(String path){}
+    public static void staticfiles(String path){
+        staticFilePath = path;
+    }
 
     public static void getDefaultResponse(String filePath, PrintWriter out, BufferedOutputStream dataOut) throws IOException {
         if (filePath.equals("/")){
             filePath = "/index.html";
         }
 
-        File file = new File("src/main/resources/static" + filePath);
+        File file = new File(staticFilePath + filePath);
 
         if (file.exists() && !file.isDirectory()) {
             String contentType = getContentType(filePath);
@@ -147,5 +162,90 @@ public class HttpServer {
         fileInputStream.read(fileData);
         fileInputStream.close();
         return fileData;
+    }
+
+    private static void handleRestRequest(String method, String filePath, URI resourceURI, PrintWriter out, Map<Integer, String> dataStore, int idCounter) {
+        String response = "";
+        String idParam = filePath.replace("/app/rest-service/", "").trim();
+
+        if (method.equals("GET")) {
+            // Convertir los valores a una lista de strings con comillas
+            StringBuilder namesJson = new StringBuilder("[ \n ");
+            for (Integer key : dataStore.keySet()) { // Iteramos sobre las claves
+                namesJson.append("\n {\"id\":")
+                        .append(key)  // Usamos directamente la clave
+                        .append(", \"name\": \"")
+                        .append(dataStore.get(key)) // Obtenemos el valor con la clave
+                        .append("\" },");
+            }
+
+            // Eliminar la última coma y cerrar el arreglo
+            if (namesJson.length() > 1) {
+                namesJson.deleteCharAt(namesJson.length() - 1);
+            }
+            namesJson.append("]");
+
+            for (Integer i = 0; i < dataStore.size(); i++){
+                System.out.println("id: " + i + ", value: " + dataStore.get(i));
+            }
+
+            // Crear la respuesta JSON
+            response = "HTTP/1.1 200 OK\r\n"
+                    + "Content-Type: application/json\r\n"
+                    + "\r\n"
+                    + "{ \"names\": " + namesJson.toString() + " }";
+        } else if (method.equals("POST")) {
+            String requestQuery = resourceURI.getQuery();
+            String newName = requestQuery.replace("name=", "").trim();
+            dataStore.put(idCounter, newName);
+
+            for (Integer i = 0; i < dataStore.size(); i++){
+                System.out.println("id: " + i + ", value: " + dataStore.get(i));
+            }
+
+            response = "HTTP/1.1 201 Created\r\n"
+                    + "Content-Type: application/json\r\n"
+                    + "\r\n"
+                    + "{\"id\":" + idCounter + ", \"name\":\"" + newName + "\"}";
+        } else if (method.equals("PUT")) {
+            String requestQuery = resourceURI.getQuery();
+            String [] params = requestQuery.split("&");
+            String newName = "";
+            Integer id = -1;
+
+            for (String param : params) {
+                String[] splitParam = param.split("=");
+                if (splitParam[0].equals("id")){
+                    id = Integer.parseInt(splitParam[1]);
+                }
+                if (splitParam[0].equals("newName")){
+                    newName = splitParam[1];
+                }
+            }
+
+            dataStore.put(id, newName);
+
+            response = "HTTP/1.1 200 Ok\r\n"
+                    + "Content-Type: application/json\r\n"
+                    + "\r\n"
+                    + "{\"id\":" + id + ", \"name\":\"" + newName + "\"}";
+        }else if (method.equals("DELETE")) {
+            String requestQuery = resourceURI.getQuery();
+            String stringId = requestQuery.replace("id=", "").trim();
+            Integer id = Integer.parseInt(stringId);
+
+            dataStore.remove(id);
+
+            response = "HTTP/1.1 200 Ok\r\n"
+                    + "Content-Type: application/json\r\n"
+                    + "\r\n";
+        } else {
+            response = "HTTP/1.1 405 Method Not Allowed\r\n"
+                    + "Content-Type: application/json\r\n"
+                    + "\r\n"
+                    + "{\"error\":\"Method not allowed\"}";
+        }
+
+        out.println(response);
     }
 }
